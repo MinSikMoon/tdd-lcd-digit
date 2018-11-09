9785dcad49dea5d7d1f2b2ee915b6c782a3418fc
minsikmoon <msmoon.business@gmail.com>
2018-11-09 10:10:24 +0900

    refactoring

--- src/main/DigitMaker.java
+++ src/main/DigitMaker.java
@@ -1,13 +1,28 @@
 package main;
 
 public class DigitMaker {
-
+	static String[] firstLines = {"._.", "..."};
 
 	public String makeDigit(int i) {
-		// TODO Auto-generated method stub
-		return  "._.\n"
-				+ "|.|\n"
-				+ "|_|";
+		if(i == 1){
+			String firstLine = firstLines[1];//"..."; 
+			String secondLine = "..|";
+			String thirdLine = "..|";
+			String[] lines = new String[]{firstLine, secondLine, thirdLine};
+			return String.join("\n", lines) + "\n";
+		}else if(i==2){
+			String firstLine = "._."; 
+			String secondLine = "._|"; 
+			String thirdLine = "|_.";
+			String[] lines = new String[]{firstLine, secondLine, thirdLine};
+			return String.join("\n", lines) + "\n";
+		}
+		
+		String firstLine = firstLines[0];//"._."; 
+		String secondLine = "|.|"; 
+		String thirdLine = "|_|";
+		String[] lines = new String[]{firstLine, secondLine, thirdLine};
+		return String.join("\n", lines) + "\n";
 	}
 	
 }
