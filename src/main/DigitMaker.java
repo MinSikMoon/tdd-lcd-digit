3b3b56acf057ab6f90c3150be58823a084dd8787
minsikmoon <msmoon.business@gmail.com>
2018-11-09 10:38:49 +0900

    수정

--- src/main/DigitMaker.java
+++ src/main/DigitMaker.java
@@ -8,24 +8,20 @@ public class DigitMaker {
 	public String makeDigit(int i) {
 		if(i/10 > 0){
 			if(i == 10){
-				String[] line1 = new String[2];
-				line1[0] = firstLines[1];
-				line1[1] = firstLines[0];
-				String firstLine = String.join(" ", line1); 
+				String[] line = new String[2];
+				line[0] = firstLines[1];
+				line[1] = firstLines[0];
+				String firstLine = String.join(" ", line); 
 				
 				String[] line2 = new String[2];
-				line2[0] = secondLines[1];
-				line2[1] = secondLines[0];
-				String secondLine = String.join(" ", line2);//secondLines[1] + " " +secondLines[0];
-				
+				line[0] = secondLines[1];
+				line[1] = secondLines[0];
+				String secondLine = String.join(" ", line);//secondLines[1] + " " +secondLines[0];
 				
 				String[] line3 = new String[2];
-				line3[0] = thirdLines[1];
-				line3[1] = thirdLines[0];
-				String thirdLine =  String.join(" ", line3);//thirdLines[1]+ " " +thirdLines[0];
-				
-				
-				
+				line[0] = thirdLines[1];
+				line[1] = thirdLines[0];
+				String thirdLine =  String.join(" ", line);//thirdLines[1]+ " " +thirdLines[0];
 				
 				String[] lines = new String[]{firstLine, secondLine, thirdLine};
 				return String.join("\n", lines) + "\n";
